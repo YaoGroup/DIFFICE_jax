a1823b081ce4e96ac26550dc2502ce2e57b8cf4c
yjwang <72957567+wangyji@users.noreply.github.com>
2024-08-05 22:48:36 -0400

    Create __init__.py

---
+++ diffice_jax/__init__.py
@@ -0,0 +1,8 @@
+from .data.pinns.preprocessing import normalize_data as normdata
+from .data.xpinns.preprocessing import normalize_data as normdatax
+
+
+from .data.pinns.sampling import data_sample_create as dsample
+from .data.xpinns.sampling import data_sample_create as dsamplex
+
+
